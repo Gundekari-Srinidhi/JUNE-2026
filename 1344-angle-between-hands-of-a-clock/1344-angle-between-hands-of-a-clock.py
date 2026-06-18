f9ec40428fc8078614ad054f397991bf12de1660
Gundekari-Srinidhi <gundekarisrinidhi@gmail.com>
2026-06-18 11:21:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (84.16%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
@@ -1,5 +1,6 @@
 class Solution:
     def angleClock(self, hour: int, minutes: int) -> float:
+        #hour hand moves 30 degree per hour and 0.5 degree per minute and minute hand moves 6 degree per minute  30(H) + 0.5(M) - 6(M)
         val = abs(30*(hour)-5.5*(minutes))
         return min(val,360-val)
         
